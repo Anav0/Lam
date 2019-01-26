d51fb169abed6f22de34a1b9717966a94294d9a6
Anav <janbul6@gmail.com>
2019-01-26 21:36:04 +0100

    Some cleanup after a while

--- Projekt/Properties/Resources.Designer.cs
+++ Projekt/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Projekt.Properties {
+namespace Lam.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Projekt.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Projekt.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Lam.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
